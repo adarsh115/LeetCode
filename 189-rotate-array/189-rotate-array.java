a3ec8b8d22045382858a2a97a0560a3348caa13b
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-03 10:15:50 +0530

    Time: 2 ms (46.78%), Space: 64.5 MB (38.69%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,14 +1,36 @@
 class Solution {
+    public void reverse(int [] nums, int s, int e){
+        if(s == e)return;
+        while(s<e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-        int ans[] = new int[nums.length];
+// METHOD 1 : O(n) time complexity & O(n) space complexity
+//         int ans[] = new int[nums.length];
         
-        for(int i = 0; i<nums.length; i++){
-            int pos = (i+k) % nums.length;
-            ans[pos] = nums[i];
-        }
+//         for(int i = 0; i<nums.length; i++){
+//             int pos = (i+k) % nums.length;
+//             ans[pos] = nums[i];
+//         }
         
-        for(int  i = 0; i<nums.length; i++ ){
-            nums[i] = ans[i];
-        }
+//         for(int  i = 0; i<nums.length; i++ ){
+//             nums[i] = ans[i];
+//         }
+// METHOD 2 : O(n) time complexity & O(1) space complexity
+        
+        if(k == 0 || nums.length == 1) return;
+        
+        k = k % nums.length;
+        int split = nums.length-k;
+        
+        reverse(nums, 0, split-1);
+        reverse(nums, split, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+        
     }
 }
