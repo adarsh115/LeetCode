51b73b9ec2d8e5cce1f9485dc7adc2ed947f866e
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-10 00:03:22 +0530

    Time: 24 ms (60.31%), Space: 45.1 MB (44.18%) - LeetHub

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ls = new ArrayList();
+        HashSet<List<Integer>> set = new HashSet();
+        
+        if(nums.length < 3)return ls;
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for(int i = 0; i< n-3; i++){
+            for(int j = i+1; j<n-2; j++){
+                
+                int two_sum = target - (nums[i] + nums[j]);
+                int start = j+1;
+                int end = n-1;
+                while(start < end){
+                    int sum = nums[start] + nums[end];
+                    if(sum == two_sum){
+                        List <Integer> list = new ArrayList();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[start]);
+                        list.add(nums[end]);
+                        // System.out.print(list);
+                        ls.add(list);
+                        
+                        while(start < end && nums[start] == nums[start+1])start++;
+                        while(start < end && nums[end] == nums[end-1])end--;
+                        
+                        end--;
+                        start++;
+                    }
+                    else if(sum > (two_sum)){
+                        end--;
+                    }
+                    else start++;
+                }
+                while(j+1 < n && nums[j] == nums[j+1])j++;
+            }
+            while(i+1 < n && nums[i] == nums[i+1])i++;
+        }
+
+        return ls;
+    }
+}
