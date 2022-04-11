cd683bba6881d3b72107d924dbd9a955fef794f4
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-11 13:02:25 +0530

    Time: 2 ms (68.83%), Space: 53.6 MB (72.15%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.java
@@ -0,0 +1,111 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        
+        // Find the middle node
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Reverse the second half
+        ListNode head2 = reverse(slow.next);
+        slow.next = null;
+        
+        // Link the two halves together
+        while (head != null && head2 != null) {
+            ListNode tmp1 = head.next;
+            ListNode tmp2 = head2.next;
+            head2.next = head.next;
+            head.next = head2;
+            head = tmp1;
+            head2 = tmp2;
+        }
+    }
+    
+    private ListNode reverse(ListNode n) {
+        ListNode prev = null;
+        ListNode cur = n;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+//     public void printll(ListNode head){
+//         ListNode temp = head;
+//         while(temp != null){
+//             System.out.print(temp.val + " ");
+//             temp = temp.next;
+//         }
+//     }
+//     public ListNode reverse(ListNode head){
+//         ListNode prev = null;
+//         ListNode currentNode = head;
+        
+//         while(currentNode != null){
+//             ListNode nextNode = currentNode.next;
+//             currentNode.next = prev;
+//             prev = currentNode;
+//             currentNode = nextNode;
+//         }
+        
+//         return prev;
+//     }
+//     public void merge(ListNode head, ListNode head2){
+       
+//         while (head != null && head2 != null) {
+//             ListNode tmp1 = head.next;
+//             ListNode tmp2 = head2.next;
+//             head2.next = head.next;
+//             head.next = head2;
+//             head = tmp1;
+//             head2 = tmp2;
+//         }
+//     }
+//     public void reorderList(ListNode head) {
+//         if(head == null || head.next == null )return;
+        
+// //         head list one
+//         ListNode headone = head;
+// //         head list two
+//         ListNode slow = head;
+        
+// //         tail list one
+//         ListNode prev = null;
+// //          tail list two
+//         ListNode fast = head;
+        
+//         while(fast!=null&&fast.next!=null){
+//             prev = slow;
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         prev.next = null;
+        
+// //         head of reversed ll
+//         ListNode headtwo = reverse(slow);
+        
+//         printll(headone);
+//         System.out.println();
+//         printll(headtwo);
+//         System.out.println();
+//         merge(headone, headtwo);
+        
+//     }
+}
