8bc0dcb1a11746be7e2c60fec1c5a34e00468a3f
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-11 13:15:44 +0530

    Time: 1 ms (99.94%), Space: 45.1 MB (88.51%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -9,83 +9,7 @@
  * }
  */
 class Solution {
-//     public void reorderList(ListNode head) {
-//         if (head == null) {
-//             return;
-//         }
-        
-//         // Find the middle node
-//         ListNode slow = head, fast = head.next;
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-        
-//         // Reverse the second half
-//         ListNode head2 = reverse(slow.next);
-//         slow.next = null;
-        
-//         // Link the two halves together
-//         while (head != null && head2 != null) {
-//             ListNode tmp1 = head.next;
-//             ListNode tmp2 = head2.next;
-//             head2.next = head.next;
-//             head.next = head2;
-//             head = tmp1;
-//             head2 = tmp2;
-//         }
-//     }
-    
-//     private ListNode reverse(ListNode n) {
-//         ListNode prev = null;
-//         ListNode cur = n;
-//         while (cur != null) {
-//             ListNode tmp = cur.next;
-//             cur.next = prev;
-//             prev = cur;
-//             cur = tmp;
-//         }
-//         return prev;
-//     }
 
-    public void reorderList(ListNode head) {
-        if(head == null || head.next == null )return;
-        
-//         head list one
-        ListNode headone = head;
-//         head list two
-        ListNode slow = head;
-        
-//         tail list one
-
-//          tail list two
-        ListNode fast = head.next;
-        
-        while(fast!=null&&fast.next!=null){
-            
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
-//         head of reversed ll
-        ListNode headtwo = reverse(slow.next);
-        slow.next = null;
-        
-        
-        printll(headone);
-        System.out.println();
-        printll(headtwo);
-        System.out.println();
-        merge(headone, headtwo);
-        
-    }
-        public void printll(ListNode head){
-        ListNode temp = head;
-        while(temp != null){
-            System.out.print(temp.val + " ");
-            temp = temp.next;
-        }
-    }
     public ListNode reverse(ListNode head){
         ListNode prev = null;
         ListNode currentNode = head;
@@ -100,7 +24,6 @@ class Solution {
         return prev;
     }
     public void merge(ListNode head, ListNode head2){
-       
         while (head != null && head2 != null) {
             ListNode tmp1 = head.next;
             ListNode tmp2 = head2.next;
@@ -111,5 +34,26 @@ class Solution {
             head = tmp1;
             head2 = tmp2;
         }
+    }
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null )return;
+        
+//         head list one
+        ListNode headone = head;
+//         head list two
+        ListNode slow = head;
+
+        ListNode fast = head.next;
+        
+        while(fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+//         head of reversed ll
+        ListNode headtwo = reverse(slow.next);
+        slow.next = null;
+
+        merge(headone, headtwo);
     }
 }
