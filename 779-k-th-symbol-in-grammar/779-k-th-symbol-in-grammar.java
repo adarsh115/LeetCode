750df6a4a7d9c2850ccc1cc73a6ae042f2f71e85
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-16 09:47:55 +0530

    Time: 0 ms (100.00%), Space: 39 MB (86.93%) - LeetHub

---
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n == 1 && k == 1)return 0;
+        int mid = (int)Math.pow(2, n-1)/2;
+        if(k <= mid) return kthGrammar(n-1, k);
+        else {
+            int ans = kthGrammar(n-1, k-mid);
+            
+            return ans == 0 ? 1:0;
+        }
+    }
+}
