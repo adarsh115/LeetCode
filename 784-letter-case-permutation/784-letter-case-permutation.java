7d68acda67cb900c020695e5b4a34c26bc14ef46
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-16 15:24:30 +0530

    Time: 5 ms (62.04%), Space: 43.3 MB (83.00%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,31 +1,36 @@
 class Solution {
-    Set<String> set = new HashSet<>();
+    
     public void solve(String ip, String op, List<String> list){
         if(ip.length() == 0){
-            if(!set.contains(op)){
+           
                 list.add(op);
-                set.add(op);
-            }
+       
+            
             return;
         }
         
 
-        String op1 = op;
-        String op2 = op;
         
         if(Character.isAlphabetic(ip.charAt(0))){
+            String op1 = op;
+            String op2 = op;
+  
             op1 = op1 + ip.substring(0,1).toUpperCase();
             op2 = op2 + ip.substring(0,1).toLowerCase();
+            
+            ip = ip.substring(1);
+            solve(ip, op1, list);
+            solve(ip, op2, list);
+            return;
         }
-        else{
-            op1 = op1 + ip.substring(0,1);
-            op2 = op2 + ip.substring(0,1);
-        }
-
-        ip = ip.substring(1);
         
+        String op1 = op;
+        op1 = op1 + ip.substring(0,1);
+        ip = ip.substring(1);
         solve(ip, op1, list);
-        solve(ip, op2, list);
+        
+        
+        
         
         return;
     }
