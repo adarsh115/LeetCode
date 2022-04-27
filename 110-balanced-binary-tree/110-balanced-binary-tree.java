4cf1ebe66ba3d8e85fe0ae06c832c2175713481e
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-27 16:07:05 +0530

    Time: 1 ms (89.97%), Space: 41.7 MB (93.11%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,26 +14,42 @@
  * }
  */
 class Solution {
+//     O(n)
+//     public int findHeight(TreeNode root){
+//         if(root == null)return 0;
+        
+//         int lh = findHeight(root.left);
+//         int rh = findHeight(root.right);
+        
+//         return 1 + Math.max(lh, rh);
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         if(root == null)return true;
+        
+//         int lh = findHeight(root.left);
+//         int rh = findHeight(root.right);
+        
+//         if(Math.abs(lh-rh) > 1) return false;
+        
+//         boolean left = isBalanced(root.left);
+//         boolean right = isBalanced(root.right);
+        
+//         if(left == false || right == false) return false;
+//         return true;
+//     }
+    
+//     O(n)
     public int findHeight(TreeNode root){
         if(root == null)return 0;
-        
         int lh = findHeight(root.left);
         int rh = findHeight(root.right);
+        
+        if(lh == -1 || rh == -1)return -1;
+        if(Math.abs(lh-rh) > 1) return -1;
         
         return 1 + Math.max(lh, rh);
     }
     public boolean isBalanced(TreeNode root) {
-        if(root == null)return true;
-        
-        int lh = findHeight(root.left);
-        int rh = findHeight(root.right);
-        
-        if(Math.abs(lh-rh) > 1) return false;
-        
-        boolean left = isBalanced(root.left);
-        boolean right = isBalanced(root.right);
-        
-        if(left == false || right == false) return false;
-        return true;
+        return (findHeight(root) != -1);
     }
 }
