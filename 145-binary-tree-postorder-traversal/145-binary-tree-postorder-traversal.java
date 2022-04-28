cd732cb984e92d861527782d3572bf03a9c32af3
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-04-28 13:15:43 +0530

    Time: 1 ms (42.12%), Space: 42.4 MB (39.04%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -14,18 +14,40 @@
  * }
  */
 class Solution {
-    public void postorder(TreeNode root, List<Integer> postorder){
-        if(root == null)return;
+//     public void postorder(TreeNode root, List<Integer> postorder){
+//         if(root == null)return;
         
-        postorder(root.left, postorder);
-        postorder(root.right, postorder);
-        postorder.add(root.val);
-    }
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+//         postorder(root.left, postorder);
+//         postorder(root.right, postorder);
+//         postorder.add(root.val);
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
         
-        postorder(root, ans);
+//         postorder(root, ans);
         
-        return ans;
+//         return ans;
+//     }
+     public List<Integer> postorderTraversal(TreeNode root) {
+         List<Integer> ans = new ArrayList<>();
+         Stack<TreeNode> stack1 = new Stack<>();
+         Stack<Integer> stack2 = new Stack<>();
+         
+         if(root == null)return ans;
+         stack1.add(root);
+         
+         while(stack1.size() > 0){
+             root = stack1.pop();
+             stack2.add(root.val);
+             
+             if(root.left != null)stack1.add(root.left);
+             if(root.right != null)stack1.add(root.right);
+         }
+         
+         while(stack2.size()>0){
+             ans.add(stack2.pop());
+         }
+         
+         return ans;
     }
 }
