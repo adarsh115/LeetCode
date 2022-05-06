d250af3d36bed7fb06e682728373642aaea9158c
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-06 17:48:11 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
@@ -41,7 +41,7 @@ class Solution
         
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i<K; i++){
-            for(int j = 0; j<K; j++){
+            for(int j = 0; j<arr[i].length; j++){
                 list.add(arr[i][j]);
             }
         }
