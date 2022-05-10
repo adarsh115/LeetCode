e1f9bbad2a1baff52fe3ac04259e12a020a77497
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-10 11:09:16 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    static int t[][] = new int[1002][1002];
+    // for(int row[] : t[][]){
+    //     Arrays.fill(row, -1);
+    // }
+    public Solution(){
+        for (int[] row : t)
+            Arrays.fill(row, -1);
+    }
+
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int w, int wt[], int val[], int n) 
+    { 
+         // your code here 
+         if(n == 0 || w == 0)return 0;
+        //  
+        //  Now we will we will check if the item's weight is less than or greatert than the weight of
+        // bag
+        // 1 . Item's weightis less than bag's weight
+        if(t[n][w] != -1)return t[n][w];
+        
+        
+        if(wt[n-1] <= w){
+              t[n][w] =  Math.max(knapSack(w, wt, val, n-1) , (val[n-1] + knapSack(w - wt[n-1], wt, val, n-1)));
+         }
+        //  2. Item's wieght is greater than bag's
+        else if(wt[n-1] > w){
+             t[n][w] =  knapSack(w, wt, val, n-1);
+        }
+        
+        return t[n][w];
+    } 
+}
+
+
