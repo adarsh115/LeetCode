1bbd52a368ec361c98fc945c634837dcf4160b42
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-11 23:57:50 +0530

    Time: 2 ms (50.28%), Space: 53.2 MB (71.62%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,24 +1,46 @@
 class Solution {
+    public boolean bfsCheck(int graph[][], int node, int color[]){
+        Queue<Integer> q = new LinkedList<>();
+
+        color[node] = 1;
+                
+        q.add(node);
+        
+
+        
+        while(!q.isEmpty()){
+            int parent = q.remove();
+            
+            for(int child : graph[parent]){
+                if(color[child] == -1){
+                    color[child] = 1-color[parent];
+                    q.add(child);
+                }
+                else if(color[child] == color[parent])return false;
+            }
+        }
+        
+        return true;
+       
+    }
 //     public boolean bfsCheck(int graph[][], int node, int color[]){
 //         Queue<Integer> q = new LinkedList<>();
-//         color[node] = 1;
-        
 //         q.add(node);
         
+//         color[node] = 1;
+        
 //         while(!q.isEmpty()){
-//             int parent = q.remove();
-            
-//             for(int child : graph[parent]){
-//                 if(child == -1){
-//                     color[child] = 1-color[parent];
-//                     q.add(child);
+//             int temp = q.remove();
+//             for(int it: graph[temp]){
+//                 if(color[it] == -1){
+//                     color[it] = 1-color[temp];
+//                     q.add(it);
 //                 }
-//                 else if(color[child] == color[parent])return false;
+//                 else if(color[it] == color[temp])return false;
 //             }
 //         }
         
 //         return true;
-       
 //     }
     
     public boolean isBipartite(int[][] graph) {
@@ -39,40 +61,7 @@ class Solution {
         return true;
         
     }
-        public boolean bfsCheck(int graph[][], int node, int color[]){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        
-        color[node] = 1;
-        
-        while(!q.isEmpty()){
-            int temp = q.remove();
-            for(int it: graph[temp]){
-                if(color[it] == -1){
-                    color[it] = 1-color[temp];
-                    q.add(it);
-                }
-                else if(color[it] == color[temp])return false;
-            }
-        }
-        
-        return true;
-    }
+
     
-//     public boolean isBipartite(int[][] graph) {
-//         int n = graph.length;
-//         int color[] = new int[n];
-        
-//         Arrays.fill(color, -1);
-        
-//         for(int i=0; i<n; i++){
-//             if(color[i] == -1){
-//                 boolean ans = bfsCheck(graph, i, color);
-//                 // System.out.print(ans);
-//                 if(ans == false)return false;
-//             }
-//         }
-//         return true;
-        
-//     }
+
 }
