4e7a7e31ce0f84bace056613445024b49f6539e5
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-11 23:54:24 +0530

    Time: 1 ms (81.21%), Space: 53.6 MB (53.61%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,5 +1,45 @@
 class Solution {
-    public boolean bfsCheck(int graph[][], int node, int color[]){
+//     public boolean bfsCheck(int graph[][], int node, int color[]){
+//         Queue<Integer> q = new LinkedList<>();
+//         color[node] = 1;
+        
+//         q.add(node);
+        
+//         while(!q.isEmpty()){
+//             int parent = q.remove();
+            
+//             for(int child : graph[parent]){
+//                 if(child == -1){
+//                     color[child] = 1-color[parent];
+//                     q.add(child);
+//                 }
+//                 else if(color[child] == color[parent])return false;
+//             }
+//         }
+        
+//         return true;
+       
+//     }
+    
+//     public boolean isBipartite(int[][] graph) {
+//        int n = graph.length;
+        
+//         int color[] = new int[n];
+        
+//         Arrays.fill(color, -1);
+        
+//         for(int i=0; i<n; i++){
+//             if(color[i] == -1){
+//                 boolean ans = bfsCheck(graph, i, color);
+                
+//                 if(ans == false)return false;
+//             }
+//         }
+        
+//         return true;
+        
+//     }
+        public boolean bfsCheck(int graph[][], int node, int color[]){
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
         
