eaf22b3f291b1de0263db08241dcf57e9dbb8b16
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-11 20:24:35 +0530

    Time: 111 ms (20.70%), Space: 53.6 MB (60.00%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        boolean t[][] = new boolean[N+1][sum+1];
+        for(int i = 0; i<N+1; i++){
+            for(int j = 0; j<sum+1; j++){
+                if(i == 0)t[i][j] = false;
+                if(j == 0)t[i][j] = true;
+            }
+        }
+        
+        for(int i = 1; i<N+1; i++){
+            for(int j = 1; j<sum+1; j++){
+               
+                if(arr[i-1] <= j){
+                    t[i][j] = t[i-1][j - arr[i-1]] || t[i-1][j];
+                }
+                
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[N][sum];
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int s : nums)sum += s;
+        
+        if(sum % 2 != 0)return false;
+        
+        return isSubsetSum(nums.length, nums, sum / 2);
+    }
+}
