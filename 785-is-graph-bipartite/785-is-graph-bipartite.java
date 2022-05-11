c534b2c600be89d06311aee60c9cb477dd13a186
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-12 00:05:35 +0530

    Time: 1 ms (81.21%), Space: 53.7 MB (48.01%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,4 +1,18 @@
 class Solution {
+    public boolean dfsCheck(int graph[][], int parent, int color[]){
+        if(color[parent] == -1)color[parent] = 1;
+        
+        for(int child : graph[parent]){
+            if(color[child] == -1){
+                color[child] = 1-color[parent];
+                
+                if(dfsCheck(graph, child, color) == false)return false;
+            }
+            else if(color[child] == color[parent])return false;
+        }
+        
+        return true;
+    }
     public boolean bfsCheck(int graph[][], int node, int color[]){
         Queue<Integer> q = new LinkedList<>();
 
@@ -33,7 +47,7 @@ class Solution {
         
         for(int i=0; i<n; i++){
             if(color[i] == -1){
-                boolean ans = bfsCheck(graph, i, color);
+                boolean ans = dfsCheck(graph, i, color);
                 
                 if(ans == false)return false;
             }
