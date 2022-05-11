e126d07796c6096823fdc37f9077e5eb5dec0a15
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-11 21:43:00 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -32,20 +32,20 @@ class Solution{
 	    // Your code goes here
 	    int m = (int)(1e9 + 7);
 	    int t[][] = new int[n+1][sum+1];
+	    
+	    
 	    for(int i=0; i<n+1; i++){
 	        for(int j=0; j<sum+1; j++){
-	            if(i==0 && j==0)t[i][j] = 1;
-	            else if(i == 0)t[i][j] = 0;
-	           // else if(j == 0)t[i][j] = 1;
-	   //     }
-	   // }
-	    
-	    
-	   //for(int i=1; i<n+1; i++){
-	   // for(int j=1; j<sum+1; j++){
+	           // if(i==0 && j==0)t[i][j] = 1;
+	            if(i == 0){
+	                if(j==0)t[i][j] = 1;
+	                else t[i][j] = 0;
+	            }
+	            
 	            else if(arr[i-1] <= j){
 	                t[i][j] = (t[i-1][j- arr[i-1]]%m + t[i-1][j]%m)%m;
 	            }
+	            
 	            else{
 	                t[i][j] = t[i-1][j]%m;
 	            }
