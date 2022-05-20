12ca3c63d2306263b18d0b7fd3afd0a7ffed19a0
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-20 13:22:11 +0530

    Time: 1233 ms (5.02%), Space: 583 MB (5.00%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
         if(s.length() <= 1)return true;
+        // s = s.toLoweCase();
+        String str = "";
         
-        int start = 0; 
-        int end = s.length() - 1;
-       
-        while(start <= end) {
-            while(start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            }
-            while(start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            }
-            if(start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                return false;
-            }
-            start++;
-            end--;
+        for(char c: s.toLowerCase().toCharArray()){
+            if(Character.isLetterOrDigit(c))
+                str += (c + "");
         }
-        return true;
+        return checkPalindrome(str);
+    }
     
-       
+    public boolean checkPalindrome(String s){
+        System.out.print(s);
+        if(s.length()<=1)return true;
+        int b = 0;
+        int e = s.length()-1;
+        
+        while(b <= e){
+            if(s.charAt(b) != s.charAt(e))return false;
+            b++;
+            e--;
+        }
+        
+        return true;
     }
 }
