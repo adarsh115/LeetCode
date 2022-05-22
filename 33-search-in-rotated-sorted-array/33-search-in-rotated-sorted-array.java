6fe8e4ffb6a03d03579e15f04981ce06588b0e8d
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-22 22:53:05 +0530

    Time: 1 ms (61.09%), Space: 43.4 MB (6.50%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,91 +1,42 @@
 class Solution {
-	public int findMin(int[] nums) {
+    public int min(int arr[], int n){
+        if(arr[0] <= arr[n-1])return 0;
+        int s = 0;
+        int e = n-1;
         
-        int l =0, len = nums.length, r = len-1;
-        int res = nums[l];
-        int prev, next;
-        while(l<=r){
-            //when array is already sorted 
-            if(nums[l] <= nums[r]){
-                return nums[l];
-            }
+        while(s <= e){
+            int mid  = s + (e-s)/2;
             
-            int mid= l+ (r-l)/2;
-            prev = (mid+len-1)%len;
-            next = (mid+1)%len;
-            
-            //mid is less then the prev and the next element
-            if(nums[mid]<= nums[prev] && nums[mid] <= nums[next]){
+            if( arr[mid] <= arr[(mid -1 +n)%n] && arr[mid] <= arr[(mid + 1)%n]){
                 return mid;
             }
-            else if(nums[mid] >= nums[l]){
-                l = mid+1;
-            }
-            else {
-                r= mid-1;
-            }
-            
+            else if( arr[mid] >= arr[0] )s = mid + 1;
+            else if( arr[mid] <= arr[n-1] )e = mid - 1;
         }
         
-        return res;
-    }
-	public int bs(int arr[],int start, int end, int target){
-		int s = start;
-		int e = end;
-		
-		while(s <= e){
-			int mid = s + (e-s)/2;
-			
-			if(arr[mid] == target){
-				return mid;
-			}
-			else if(arr[mid] < target ){
-				s = mid+1;
-			}
-			else{
-				e = mid-1;
-			}
-		}
-		return -1;
-	}
-
-
-    public int search(int[] nums, int target) {
-        int arrayStartIndex = findRotation(nums);
-        int index= binarySearch(nums, arrayStartIndex,nums.length-1,target);
-        int index2 = binarySearch(nums, 0,arrayStartIndex-1,target);
-        return index==-1?index2:index;
-        
-    }
-    
-     public int findRotation(int [] arr){
-        int N = arr.length;
-        int start = 0,end = N-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            int prev = (mid - 1 + N)%N;
-            int next = (mid + 1)%N;
-            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next])
-                return mid;
-            else if(arr[mid]>=arr[0])
-                start = mid+1;
-            else 
-                end = mid-1;
-        }
-        return 0;
-    }
-    
-    public int binarySearch(int [] arr,int start , int end,int target){
-        
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(arr[mid] == target)
-                return mid;
-            else if(arr[mid]>=target)
-                end = mid-1;
-            else
-                start = mid + 1;
-        }
         return -1;
+    }
+    public int binarySearch(int arr[], int s, int e, int target){
+        
+        while(s <= e){
+            int mid = s+ (e-s)/2;
+            
+            if(arr[mid] == target)return mid;
+            else if(arr[mid] < target)s = mid+1;
+            else {
+                e = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        int min = min(nums, nums.length);
+        // System.out.print(min);
+        int ans1 = binarySearch(nums, 0, min-1, target);
+        int ans2 = binarySearch(nums, min, nums.length-1, target);
+        
+        return Math.max(ans1, ans2);
+        // return -1;
     }
 }
