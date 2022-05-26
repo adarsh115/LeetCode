159dc82a22d4b3a2dacd872b09454214741d7c35
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-27 01:36:40 +0530

    Time: 3 ms (39.10%), Space: 44.9 MB (6.28%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+//     Set<Integer> hash = new HashSet<>();
+//     List<List<Integer>> res = new ArrayList<>();
+//     int[] nums;
+    
+//     public void solve(List<Integer> l, int p, int N) {
+//         if (p == N) {
+//             int h = l.hashCode();
+//             if (!hash.contains(h)) {
+//                 hash.add(h);
+//                 res.add(new ArrayList<>(l));
+//             }
+//             return;
+//         }
+        
+//         l.add(nums[p]);
+//         solve(l, p+1, N);
+//         l.remove(l.size()-1);
+//         solve(l, p+1, N);
+//     }
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+//         List<Integer> subsets = new ArrayList<>();
+//         this.nums = nums;
+//         solve(subsets,  0, nums.length);
+        
+//         return res;
+//     }
+    
+        Set<Integer> hash = new HashSet<>();
+    List<List<Integer>> res = new ArrayList<>();
+    int n;
+    int[] nums;
+    
+    public void search(List<Integer> l, int p) {
+        if (p == n) {
+            int h = l.hashCode();
+            if (!hash.contains(h)) {
+                hash.add(h);
+                res.add(new ArrayList<>(l));
+            }
+            return;
+        }
+        l.add(nums[p]);
+        search(l, p+1);
+        l.remove(l.size()-1);
+        search(l, p+1);
+    }
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        this.n = nums.length; this.nums = nums;
+        Arrays.sort(nums);
+        search(new ArrayList<Integer>(), 0);
+        return res;
+    }
+}
