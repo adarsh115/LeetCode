6ac729905c91fb928ae3ee74c18b0f331d6563d8
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-30 17:03:01 +0530

    Time: 2 ms (21.67%), Space: 44.1 MB (32.52%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -35,6 +35,26 @@ class Solution {
 
     }
     public boolean isValidBST(TreeNode root) {
-        return dfs(root, null, null);
+        if(root == null)return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            
+            root = stack.pop();
+            
+            if(pre != null && root.val <= pre.val)return false;
+            
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
+    // public boolean isValidBST(TreeNode root) {
+    //     return dfs(root, null, null);
+    // }
 }
