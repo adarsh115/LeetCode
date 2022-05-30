d844601767b1402d7a905afc0ab7bad2d710f0ea
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-30 13:57:08 +0530

    Time: 1 ms (7.55%), Space: 41.7 MB (39.98%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,25 +15,34 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-//         if(p == null &&  q == null)return true;
-//         if(p == null || q == null)return false;
-       
-//         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        Queue<TreeNode> qu = new LinkedList<>();
-        qu.add(p);
-        qu.add(q);
+        if(p == null && q != null || p != null && q == null)return false;
         
-        while(qu.size()>0){
-            p = qu.remove();
-            q = qu.remove();
+        if(p == null && q == null)return true;
+        Queue<TreeNode> same = new LinkedList<>();
+        
+        same.add(p);
+        same.add(q);
+        
+        while(!same.isEmpty()){
+        
+            TreeNode first = same.remove();
+            TreeNode second = same.remove();
             
-            if(p == null && q == null)continue;
-            else if (q == null || p == null || p.val != q.val)return false;
+            if(first.val != second.val)return false;
+            if(first.left != null && second.left == null || first.left == null && second.left != null)return false;
+            if(first.right != null && second.right == null || first.right == null && second.right != null)return false;
             
-            qu.add(p.left);
-            qu.add(q.left);
-            qu.add(p.right);
-            qu.add(q.right);
+            if(first.left != null && second.left != null){
+                same.add(first.left);
+                same.add(second.left);
+            }
+
+            
+            if(first.right != null && second.right != null){
+                same.add(first.right);
+                same.add(second.right);
+            }
+
         }
         
         return true;
