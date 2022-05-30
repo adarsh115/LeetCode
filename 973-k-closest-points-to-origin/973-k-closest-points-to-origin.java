acc1e57c8f61b4b034817856d68f640a53e56f84
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-05-30 20:44:45 +0530

    Time: 63 ms (29.04%), Space: 117.2 MB (45.03%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,34 +1,33 @@
 class Pair{
-    int key;
-    int point[] = new int[2];
-    Pair(int d, int p[]){
-        key = d;
+    int point[];
+    int dist;
+    
+    Pair(int p[], int _d){
+        dist = _d;
         point = p;
     }
 }
-
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Queue<Pair> q = new PriorityQueue<>((a,b) -> {return b.key-a.key;});
+        Queue<Pair> q = new PriorityQueue<>((a,b) -> {return b.dist - a.dist;});
         
-        for(int arr[] : points){
-            int x = arr[0];
-            int y = arr[1];
+        for(int point[]: points){
+            int x = point[0]; int y = point[1];
+            int distance = x*x + y*y;
             
-            int distance_from_origin = x*x + y*y;
+            q.add(new Pair(point, distance));
             
-            q.add(new Pair(distance_from_origin, arr));
-            
-            if(q.size() > k)q.remove();
+            if(q.size() > k){
+                q.remove();
+            }
         }
         
         int ans[][] = new int[q.size()][2];
-        
+
         int i = 0;
         while(!q.isEmpty()){
             ans[i++] = q.remove().point;
         }
-        
         return ans;
     }
 }
