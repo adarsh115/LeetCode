8b4c9edb958a896fa6693c1878d8ccb8574f5ee0
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-05 00:06:22 +0530

    Time: 9 ms (34.49%), Space: 46.6 MB (17.56%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,10 +1,10 @@
 class Solution {
     List<List<String>> res = new ArrayList<>();
-    Set<Integer> col = new HashSet<>();
-    Set<Integer> negD = new HashSet<>();
-    Set<Integer> posD = new HashSet<>();
+    HashSet<Integer> col = new HashSet<>();
+    HashSet<Integer> negD = new HashSet<>();
+    HashSet<Integer> posD = new HashSet<>();
     
-    public void solve(int n, int r, String board[][]){
+    public void solve(int n, int r, String[][] board){
         if(r == n){
             List<String> list= new ArrayList<>();
             for(String row[] : board){
@@ -12,33 +12,36 @@ class Solution {
                 list.add(currentRow);
             }
             
-            res.add(new ArrayList(list));
+            res.add(list);
             return;
         }
         
-        for(int c = 0; c < n; c++){
-            if(col.contains(c) || negD.contains(r-c) || posD.contains(c+r))continue;
+        for(int c = 0; c<n ; c++){
+            if(col.contains(c) || negD.contains(r-c) || posD.contains(r+c))continue;
             
-            col.add(c); negD.add(r-c); posD.add(r+c);
+            col.add(c);
+            negD.add(r-c);
+            posD.add(r+c);
+            
             board[r][c] = "Q";
-            
             solve(n, r+1, board);
             
-                           
-            col.remove(c); negD.remove(r-c); posD.remove(r+c);
             board[r][c] = ".";
+            col.remove(c);
+            negD.remove(r-c);
+            posD.remove(r+c);
             
         }
     }
     
     public List<List<String>> solveNQueens(int n) {
-        String board[][] = new String[n][n];
-        for(String b[] : board){
-            Arrays.fill(b, ".");
+        String[][] board = new String[n][n];
+        
+        for(String row[] : board){
+            Arrays.fill(row, ".");
         }
         
         solve(n, 0, board);
-        
         return res;
     }
 }
