4751f666e2b56bf4e6c4da9b5ea4d0f8bcba6bdf
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-05 18:04:09 +0530

    Time: 4 ms (56.83%), Space: 39.8 MB (81.31%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    Set<Integer> col  = new HashSet<Integer>();
+    Set<Integer> nDia  = new HashSet<Integer>();
+    Set<Integer> pDia  = new HashSet<Integer>();
+    
+    int count = 0;
+    
+    public void solve(int board[][], int r, int n){
+        if(r == n){
+            count++;
+            return;
+        }
+        
+        for(int c=0; c<n; c++){
+            if(col.contains(c) || nDia.contains(r-c) || pDia.contains(r+c))continue;
+            
+            col.add(c); nDia.add(r-c); pDia.add(r+c);
+            
+            board[r][c] = 1;
+            
+            solve(board, r+1, n);
+            
+            col.remove(c); nDia.remove(r-c); pDia.remove(r+c);
+            
+            board[r][c] = 0;
+        }
+        
+    }
+    
+    public int totalNQueens(int n) {
+        int board[][] = new int[n][n];
+        
+        for(int b[] : board){
+            Arrays.fill(b, 0);
+        }
+        
+        solve(board, 0, n);
+        
+        return count;
+    }
+}
