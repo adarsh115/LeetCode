b07a75a4446d6939ce0587afb5a0c7bb357fdf14
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-07 12:58:14 +0530

    Time: 3 ms (5.31%), Space: 43 MB (36.63%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,35 +1,37 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int result[] = new int[m+n];
+        // int result[] = new int[m+n];
         
         int i = 0;
         int j = 0;
-        int k = 0;
+        // int k = 0;
         
         while(i < m && j < n){
             if(nums1[i] <= nums2[j]){
-                result[k] = nums1[i];
+                // result[k] = nums1[i];
                 i++;
             }
             else{
-                result[k] = nums2[j];
-                j++;
+                int temp = nums1[i];
+                nums1[i] = nums2[j];
+                nums2[j] = temp;
+                i++;
+                
+                Arrays.sort(nums2);
             }
-            k++;
+            // k++;
         }
 
-        while(i < m){
-            result[k++] = nums1[i++];
-        }
+
         while(j < n){
-            result[k++] = nums2[j++];
+            nums1[i++] = nums2[j++];
         }
         
-        for(i = 0; i<nums1.length; i++){
-            nums1[i] = result[i];
-        }
+        // for(i = 0; i<nums1.length; i++){
+        //     nums1[i] = result[i];
+        // }
         
-                System.out.print(Arrays.toString(result));
+                // System.out.print(Arrays.toString(result));
         // nums1  = result;
     }
 }
