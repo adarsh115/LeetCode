82518c70d55f7dbf97a765d7f2ca5372d274528d
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-08 21:17:36 +0530

    Added solution - LeetHub

---
+++ Leap Year - GFG/leap-year.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isLeap(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int isLeap(int year){
+        //code here
+       if(year % 400 == 0)
+        {
+            return 1;
+        }
+        else if (year % 100 == 0)
+        {
+                return 0;
+        }
+        else if(year % 4 == 0)
+        {
+            return 1;
+        }
+        else
+        {
+                return 0;
+        }
+    }
+}
