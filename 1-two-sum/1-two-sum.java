fdc132e04bd4d71053f81b9979bb0f81ec79e19f
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-16 02:27:21 +0530

    Time: 1 ms (99.65%), Space: 42.4 MB (88.07%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> set = new HashMap<>();
         
+        //1. Brute force - In this approach we will be looping though array twice
+        //                 as soon as we find the sum we will return the index of two numbers
+        
+        //2. HashSet - We will check for two numbers whose sum equals target
+        
+    
+       Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++){
-            if(!set.containsKey(target - nums[i]))set.put(nums[i], i);
-            else{
-                int ione = set.get(target - nums[i]);
-                return new int[]{ione, i};
+            
+            int current = nums[i];
+            int diff = (-current + target);
+            
+            if(map.containsKey(diff)){
+                return new int[]{i, map.get(diff)};
             }
+            
+            map.put(current, i);
         }
+        
         return new int[]{-1,-1};
+        
     }
 }
