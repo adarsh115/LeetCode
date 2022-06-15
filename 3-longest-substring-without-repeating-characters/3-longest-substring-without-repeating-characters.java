9d4e66cb815fa4fff970863d2ef43bb0edf625c4
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-16 02:41:35 +0530

    Time: 21 ms (23.29%), Space: 44.4 MB (61.37%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,36 +1,45 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
         Map<Character, Integer> map = new HashMap<>();
         
         int i = 0;
-        int j = 0;
+        int j =0;
         
-        int maxLength = 0;
-        while(j < s.length()){
-            if(map.containsKey(s.charAt(j))){
-                map.put(s.charAt(j), map.get(s.charAt(j)) + 1);
-            }
-            else
-                map.put(s.charAt(j), 1);
+        int max = 0;
+        
+        while(j < len){
             
-            int k = j-i+1;
-            if(map.size() > k){
+            char currentChar = s.charAt(j);
+            
+            if(map.containsKey(currentChar)){
+                map.put(currentChar, map.get(currentChar)+1);
+            }
+            else{
+                map.put(currentChar,1);
+            }
+            
+            if(map.size() == j - i + 1){
+                max = Math.max(max, j-i+1);
                 j++;
             }
-            else if(map.size() == k){
-                maxLength = Math.max(maxLength, k);
-                j++;
-            }
-            else if(map.size() < k){
+            else if(map.size() < j-i+1){
+                
                 while(map.size() < j-i+1){
-                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-                    if(map.get(s.charAt(i)) == 0)map.remove(s.charAt(i));
+                    
+                    if(map.containsKey(s.charAt(i))){
+                        map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                    }
+                    if(map.get(s.charAt(i)) == 0){
+                        map.remove(s.charAt(i));
+                    }
                     i++;
                 }
                 j++;
             }
+            
         }
         
-        return maxLength;
+        return max;
     }
 }
