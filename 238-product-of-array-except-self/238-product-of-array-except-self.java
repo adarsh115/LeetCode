e1348cf9c9f19762eed27382266cbce98e20e332
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-25 02:53:08 +0530

    Time: 33 ms (5.96%), Space: 62.2 MB (6.42%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -4,20 +4,31 @@ class Solution {
         int product[] = new int[nums.length];
         
         int pre = 1;
+        int prefix[] = new int[nums.length];
         for(int i=0; i<nums.length; i++){
-            product[i] = pre;
             pre = pre*nums[i];
+            prefix[i] = pre;
+            
         }
         
         int post = 1;
-        
+        int postfix[] = new int[nums.length];
         for(int i = nums.length-1; i>=0; i--){
-            product[i] = product[i]*post;
-            
             post = post*nums[i];
+            postfix[i] = post;
+            
         }
         
-        // System.out.print(Arrays.toString(product));
+        System.out.print(Arrays.toString(prefix));
+        System.out.print(Arrays.toString(postfix));
+        
+        // int prod = 1;
+        
+        for(int i =0 ; i<nums.length-1; i++){
+            if(i == 0){product[i] = postfix[i+1];continue;}
+            product[i] = prefix[i-1]*postfix[i+1];
+        }
+        product[nums.length-1] = prefix[nums.length-2];
         
         return product;
     }
