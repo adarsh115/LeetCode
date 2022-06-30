adf68e32d6ade8d53d8da20b336fff7370626507
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-06-30 16:07:56 +0530

    Time: 6 ms (12.74%), Space: 44.8 MB (46.63%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -0,0 +1,36 @@
+class Solution {
+    // boolean visited[];
+    
+//     public void solve(List<List<Integer>> rooms, HashSet<Integer> keys, int room){
+//         visited[room] = true;
+        
+//         for(int key : rooms.get(room)){
+//             solve(rooms, keys, key);
+//         }
+//         return;
+//     }
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean visited[] = new boolean[rooms.size()];
+        
+        HashSet<Integer> keys = new HashSet<>();   
+        keys.add(0);
+        
+        // solve(rooms, keys, 0);
+        Stack<Integer> stack = new Stack<>();
+        stack.add(0);
+        
+        while(!stack.isEmpty()){
+            int key = stack.pop();
+            visited[key] = true;
+            
+            for(int key_in_room : rooms.get(key)){
+                if(!visited[key_in_room])stack.push(key_in_room);
+            }
+        }
+        
+        for(boolean x : visited){
+            if(x == false)return false;
+        }
+        return true;
+    }
+}
