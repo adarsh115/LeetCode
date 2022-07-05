101ee3f9b4fc9a91f14b5611bc12b8a775f8df78
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-05 19:32:31 +0530

    Time: 1 ms (11.15%), Space: 40.6 MB (65.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,14 @@
 class Solution {
-    public int climbStairs(int n) {
-        if(n == 1)return 1;
-        int prev = 1;
-        int prev2 = 2;
-        for(int i = 3; i<=n; i++){
-            int current = prev + prev2;
-            
-            prev = prev2;
-            prev2 = current;
-        }
+    int dp[];
+    public int solve(int n){
+        if(n <= 2)return n;
         
-        return prev2;
+        if(dp[n] != -1)return dp[n];
+        return dp[n] =  solve(n-1) + solve(n-2);
+    }
+    public int climbStairs(int n) {
+        this.dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n);
     }
 }
