6970d532cf77016d05b334e83a20001e6e0d3d11
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-07 19:20:32 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (65.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
     int dp[];
+    
     public int solve(int n){
         if(n <= 2)return n;
         
         if(dp[n] != -1)return dp[n];
+        
         return dp[n] = solve(n-1) + solve(n-2);
     }
     public int climbStairs(int n) {
         this.dp = new int[n+1];
         Arrays.fill(dp, -1);
-        
         return solve(n);
     }
 }
