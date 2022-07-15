b587d03b5b0b05584addf71df8d7c649c0350b4e
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-15 20:29:02 +0530

    Time: 1 ms (72.34%), Space: 41.6 MB (65.66%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,34 +1,28 @@
 class Solution {
-    
-    public List<Integer> solve(int N, List<List<Integer>> ans){
-        if(N == 1){
-            List<Integer> base = new ArrayList<>();
-            base.add(1);
-            
-            ans.add(base);
-            return base;
-        }
-        
-        
-        List<Integer> previous = solve(N-1,ans);
-        List<Integer> current = new ArrayList<>();
-        
-        current.add(0, previous.get(0));
-        for(int i = 1; i<N-1;i++){
-            int curr = previous.get(i) + previous.get(i-1);
-            current.add(i, curr);
-        }
-        current.add(N-1, previous.get(previous.size() -1));
-    
-        ans.add(current);
-        
-        return current;
-    }
-      
+
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
-        solve(numRows, ans);
-        
-        return ans;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        ArrayList<Integer> pre = null;
+
+        for (int i = 1; i <= numRows; i++) {
+            ArrayList<Integer> current = new ArrayList<>();
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i)current.add(1); 
+                else current.add(pre.get(j - 1) + pre.get(j - 2));
+            }
+            result.add(current);
+            pre = current;
+        }
+        // for (int i = 1; i <= numRows; i++) {
+        //     ArrayList<Integer> save = new ArrayList<>();
+        //     for (int j = 1; j <= i; j++)
+        //         if (j == 1 || j == i) save.add(1);
+        //         else save.add(pre.get(j-1) + pre.get(j-2));
+        //     result.add(save);
+        //     pre = save;
+        // }
+        // return result;
+        return result;
     }
 }
