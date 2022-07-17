957af7d94e8b5e2cd4dada956ef4a0222d6a9324
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-17 12:02:25 +0530

    Time: 3 ms (78.30%), Space: 47.6 MB (62.12%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,57 +10,48 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pointer1 = l1;
-        ListNode pointer2 = l2;
         
-        ListNode dummy = new ListNode(100);
-        
+        ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
         
-        int sum = 0;
         int rem = 0;
-        int val = 0;
+        int sum = 0;
         
-        while(pointer1 != null && pointer2 != null){
-            sum = pointer1.val + pointer2.val + rem;
-            val = sum % 10;
-            rem = sum / 10;
-            
-            ListNode newnode = new ListNode(val);
+        while(l1 != null && l2 != null){
+            sum = l1.val + l2.val + rem;
+            rem = sum/10;
+            ListNode newnode = new ListNode(sum % 10);
             temp.next = newnode;
             temp = temp.next;
             
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
         
-        while(pointer1 != null){
-            sum = pointer1.val + rem;
-            val = sum % 10;
-            rem = sum / 10;
-            
-            ListNode newnode = new ListNode(val);
+        while(l1 != null){
+            sum = l1.val + rem;
+            rem = sum/10;
+            ListNode newnode = new ListNode(sum % 10); 
             temp.next = newnode;
             temp = temp.next;
             
-            pointer1 = pointer1.next;
+            l1 = l1.next;
         }
-        while(pointer2 != null){
-            sum = pointer2.val + rem;
-            val = sum % 10;
-            rem = sum / 10;
-            
-            ListNode newnode = new ListNode(val);
+        while(l2 != null){
+            sum = l2.val + rem;
+            rem = sum/10;
+            ListNode newnode = new ListNode(sum % 10); 
             temp.next = newnode;
             temp = temp.next;
-         
-            pointer2 = pointer2.next;
+            
+            l2 = l2.next;
         }
+        
         if(rem != 0){
-            ListNode newnode = new ListNode(rem);
+            ListNode newnode = new ListNode(rem); 
             temp.next = newnode;
-            temp = temp.next;
         }
+        
         
         return dummy.next;
     }
