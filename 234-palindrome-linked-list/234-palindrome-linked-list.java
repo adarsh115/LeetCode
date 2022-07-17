bd0b920d8c9de04f48569ef51b626c94a7fbf0c0
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-17 19:54:04 +0530

    Time: 16 ms (38.95%), Space: 99.2 MB (40.39%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,45 +9,49 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) return true;
-
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null)return head;
         
-        int n = 0;
+        ListNode rev = reverse(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+        
+        return rev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null)return true;
+        
         ListNode temp = head;
+        
+        int n=0;
+        // ListNode temp = head;
         while(temp != null){
+            temp = temp.next;
             n++;
-            temp=temp.next;
         }
         
         ListNode slow = head;
         ListNode fast = head.next;
+        
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
         
+        ListNode head1 = head;
+        ListNode head2 = (n % 2 == 0) ? reverse(slow.next) : reverse(slow);
         
-        ListNode l1 = head;
-        ListNode l2 = (n%2 == 0) ? reverse(slow.next) : reverse(slow);
         slow.next = null;
+        // System.out.print(slow.val);
+    
+        while(head1 != null && head2 != null){
+                // System.out.print(head1.val + " " + head2.val);
+            if(head1.val != head2.val)return false;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
         
-        while(l1 != null && l2 != null){
-            if(l1.val != l2.val)return false;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
         return true;
-    }
-    public ListNode reverse(ListNode head){
-        ListNode prev = null;
-        ListNode current = head;
-        while(current != null){
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
         }
-        return prev;
-    }
 }
