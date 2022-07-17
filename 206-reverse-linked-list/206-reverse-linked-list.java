2a3b17fc16e015e90dceead53209333af0c6fc24
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-17 10:59:43 +0530

    Time: 1 ms (12.22%), Space: 42.7 MB (64.96%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    ListNode reverse(ListNode head){
+    public ListNode reverse(ListNode head){
         if(head == null || head.next == null)return head;
         
         ListNode rev = reverse(head.next);
@@ -21,14 +21,15 @@ class Solution {
     }
     public ListNode reverseList(ListNode head) {
 //         ListNode prev = null;
-//         ListNode curr = head;
+//         ListNode current = head;
         
-//         while(curr != null){
-//             ListNode next = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = next;
+//         while(current != null){
+//             ListNode nextNode = current.next;
+//             current.next = prev;
+//             prev = current;
+//             current = nextNode;
 //         }
+        
         return reverse(head);
     }
 }
