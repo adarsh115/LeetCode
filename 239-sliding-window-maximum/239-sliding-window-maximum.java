1f031fbf4607d2f0ad205b5d5933a7b6038df35b
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-20 13:19:18 +0530

    Time: 33 ms (91.92%), Space: 56.8 MB (98.29%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if(n <= k){
+            int m = Integer.MIN_VALUE;
+            for(int x : nums)m = Math.max(m, x);
+            
+            return new int[]{m};
+        }
+        
+        
+        int maxie[] = new int[n-k+1];
+        Deque<Integer> list = new ArrayDeque<Integer>();
+        // Deque<Integer> win = new ArrayDeque<>();
+        // Queue<Integer> list = new PriorityQueue<Integer>((a,b) -> {return b - a;});
+        
+        int i = 0;
+        int j = 0;
+
+        while(j<n){
+            if(list.size() == 0)list.offer(nums[j]);
+            else{
+                while(list.size() > 0 && list.peekLast() < nums[j])list.pollLast();
+                list.offer(nums[j]);
+            }
+            
+            if(j-i+1 == k){
+                maxie[i] = list.peekFirst();
+                if(nums[i] == list.peekFirst()){
+                    list.pollFirst();
+                }
+                i++;
+                j++;
+            }
+            else if(j-i+1 < k){
+                j++;
+            }
+
+        }
+        
+        return maxie;
+    }
+}
