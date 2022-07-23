f5f07b6fe3b517d41c1e1fad333bb881e0b8c383
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-23 23:22:11 +0530

    Time: 0 ms (100.00%), Space: 42 MB (89.18%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,39 +14,24 @@
  * }
  */
 class Solution {
+    //     private boolean helper(TreeNode root, long min, long max){
+    //   if(root == null) return true;
+    //   boolean isLeftValid = helper(root.left, min, root.val);
+    //   boolean isRightValid = helper(root.right, root.val, max);
+    //   return root.val < max && root.val > min && isLeftValid && isRightValid;
+    // }
+    public boolean solve(TreeNode root, long floor, long ceil){
+        if(root == null)return true;
+        
+        boolean left = solve(root.left, floor, root.val);
+        boolean right = solve(root.right, root.val, ceil);
+        
+        
+        return root.val < ceil && root.val > floor && left && right;
 
+    }
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return solve(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        // return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        
-        boolean left = isValidBST(root.left, minVal, root.val);
-        boolean right = isValidBST(root.right, root.val, maxVal);
-        
-        return left && right;
-    }
-//     public boolean isValidBST(TreeNode root) {
-//         if(root == null)return true;
-//         Stack<TreeNode> stack = new Stack<>();
-//         TreeNode pre = null;
-        
-//         while(root != null || !stack.isEmpty()){
-//             while(root != null){
-//                 stack.push(root);
-//                 root = root.left;
-//             }
-            
-//             root = stack.pop();
-            
-//             if(pre != null && root.val <= pre.val)return false;
-            
-//             pre = root;
-//             root = root.right;
-//         }
-//         return true;
-//     }
-
 }
