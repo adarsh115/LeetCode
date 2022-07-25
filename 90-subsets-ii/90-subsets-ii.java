c3e371049e877820a07fbf5fcf6f5145400878d0
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-25 19:31:48 +0530

    Time: 4 ms (27.35%), Space: 44.7 MB (12.80%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,57 +1,31 @@
 class Solution {
-    Set<Integer> hash = new HashSet<>();
-    List<List<Integer>> res = new ArrayList<>();
-    int[] nums;
+    HashSet<Integer> codes = new HashSet<>();
+    List<List<Integer>> subsets = new ArrayList<>();
     
-    public void solve(List<Integer> l, int p, int N) {
-        if (p == N) {
-            int h = l.hashCode();
-            if (!hash.contains(h)) {
-                hash.add(h);
-                res.add(new ArrayList<>(l));
+    public void solve(int nums[], int i, int N, List<Integer> list){
+        if(i == N){
+            int hashcode = list.hashCode();
+            
+            if(!codes.contains(hashcode)){
+                subsets.add(new ArrayList(list));
+                codes.add(hashcode);
+                return;
             }
+            
             return;
         }
         
-        l.add(nums[p]);
-        solve(l, p+1, N);
-        l.remove(l.size()-1);
-        solve(l, p+1, N);
+        list.add(nums[i]);
+        solve(nums, i+1, N, list);
+        
+        list.remove(list.size()-1);
+        solve(nums, i+1, N, list);    
+  
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
-        List<Integer> subsets = new ArrayList<>();
-        this.nums = nums;
+        int n = nums.length;
         Arrays.sort(nums);
-        solve(subsets,  0, nums.length);
-        
-        return res;
+        solve(nums, 0, n, new ArrayList<>());
+        return subsets;
     }
-    
-//         Set<Integer> hash = new HashSet<>();
-//     List<List<Integer>> res = new ArrayList<>();
-//     int n;
-//     int[] nums;
-    
-//     public void search(List<Integer> l, int p) {
-//         if (p == n) {
-//             int h = l.hashCode();
-//             if (!hash.contains(h)) {
-//                 hash.add(h);
-//                 res.add(new ArrayList<>(l));
-//             }
-//             return;
-//         }
-//         l.add(nums[p]);
-//         search(l, p+1);
-//         l.remove(l.size()-1);
-//         search(l, p+1);
-//     }
-    
-//     public List<List<Integer>> subsetsWithDup(int[] nums) {
-//         this.n = nums.length; this.nums = nums;
-//         Arrays.sort(nums);
-//         search(new ArrayList<Integer>(), 0);
-//         return res;
-//     }
 }
