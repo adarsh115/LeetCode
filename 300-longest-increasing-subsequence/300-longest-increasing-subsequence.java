305257bccdc737096fc06276cf0b7d1d147e2f9d
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-07-28 14:36:49 +0530

    Time: 128 ms (13.65%), Space: 109.4 MB (8.94%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -20,10 +20,24 @@ class Solution {
         
         int n = nums.length;
         this.dp = new int[n+1][n+2];
-        for(int arr[] : dp){
-            Arrays.fill(arr, -1);
+//         for(int arr[] : dp){
+//             Arrays.fill(arr, -1);
+//         }
+        
+        for(int i = n-1; i>=0; i--){
+            for(int p = i-1; p>=-1; p--){
+                int not_take = dp[i+1][p+1];
+
+                int take = 0;
+                if(p == -1 || nums[i] > nums[p]){
+                    take = 1+dp[i+1][i+1];
+                }
+                
+                dp[i][p+1] = Math.max(not_take, take);
+            }
         }
         
-        return solve(nums, 0, n, -1);
+        return dp[0][0];
+        // return solve(nums, 0, n, -1);
     }
 }
