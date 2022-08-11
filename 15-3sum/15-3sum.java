903c6d2c60139d5a92ac93c10994e752145a84b7
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-08-12 00:16:53 +0530

    Time: 28 ms (79.10%), Space: 60.9 MB (27.97%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,54 +1,51 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //         Arrays.sort(nums);
-        // List<List<Integer>> list = new ArrayList<>();
-        // for(int i = 0; i < nums.length; i++){
-        //     for(int j = i + 1; j < nums.length; j++){
-        //         for(int k = j + 1; k < nums.length; k++){
-        //             if(nums[i] + nums[j] + nums[k] == 0){
-        //                 ArrayList<Integer> nums2 = new ArrayList<Integer>();
-        //                 nums2.add(nums[i]);
-        //                 nums2.add(nums[j]);
-        //                 nums2.add(nums[k]);
-        //                 if(!list.contains(nums2)) list.add(nums2);
-        //             }
-        //         }
-        //     }
-        // }
-        // return list;
         
-        List<List<Integer>> allTriplets = new ArrayList<>();
         int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        
-        for(int i = 0; i<=n-3; i++){
-            if(i > 0 && nums[i] == nums[i-1])continue;
-            
-            int target = -nums[i];
-            int j = i+1;
-            int k = n-1;
-            
-            while(j < k){
-                int sum = nums[j] + nums[k];
+        // System.out.print(Arrays.toString(nums));
+        int i=0;
+        while(i <= n-3){
+            if(i == 0 || nums[i] != nums[i-1]){
                 
-                if(sum > target)k--;
-                else if(sum < target)j++;
-                else{
+                int target = -nums[i];
+                
+                int s = i+1;
+                int e = n-1;
+                
+                // System.out.print(target);
+                while(s < e){
                     
-                    List<Integer> triplets = new ArrayList<>();
+                    int sum = nums[s] + nums[e];
+                    if(sum == target){
+                        ArrayList<Integer> triplet = new ArrayList<>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[s]);
+                        triplet.add(nums[e]);
 
-                    triplets.add(nums[i]); triplets.add(nums[j]); triplets.add(nums[k]);
-                    allTriplets.add(triplets);
-                    
-                    while(j<k && nums[j] == nums[j+1])j++;
-                    while(j<k && nums[k] == nums[k-1])k--;
-                    
-                    j++;
-                    k--;
+                        
+                        list.add(new ArrayList<>(triplet));
+                        
+                        while(s < e && nums[s] == nums[s+1])s++;
+                        while(s < e && nums[e] == nums[e-1])e--;
+                        
+                        s++;
+                        e--;
+                    }
+                    else if(sum < target){
+                        s++;
+                    }
+                    else{
+                        e--;
+                    } 
                 }
+                i++;
+            }
+            else{
+                i++;
             }
         }
-        
-        return allTriplets;
+        return list;
     }
 }
