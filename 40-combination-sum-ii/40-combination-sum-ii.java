80aa294fe02db459d4b0a18fd48da490cc45e728
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-08-11 22:55:33 +0530

    Time: 11 ms (16.67%), Space: 44.3 MB (29.21%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,36 +1,37 @@
 class Solution {
     
     List<List<Integer>> combination = new ArrayList<>();
-    int arr[];
+    int size;
+    
     HashSet<Integer> codes = new HashSet<>();
     
-    public void solve(int index, int N, int target, ArrayList<Integer> list){
-     
+    public void solve(int[] candidates, int i, int target, List<Integer> list){
+        if(i == size || target <= 0){
+            
             if(target == 0){
                 int code = list.hashCode();
-                if(!codes.contains(code)){combination.add(new ArrayList(list));codes.add(code);}
+                
+                if(!codes.contains(code)){
+                    combination.add(new ArrayList<>(list));
+                    codes.add(code);
+                }
                 return;
             }
-            if(index == N)return;
-        
-        
-        	if(arr[index] <= target) {
-            // include
-            list.add(arr[index]);    
-            solve(index+1, N, target-arr[index], list);
-            list.remove(list.size()-1);
-            // Skip all duplicate entries
-            while(index+1 < N && arr[index] == arr[index+1]) index += 1;
-            solve(index+1, N, target, list);
+            return;
         }
-
         
+        list.add(candidates[i]);
+        solve(candidates, i+1, target-candidates[i], list);
+        list.remove(list.size()-1);
+        
+        while(i+1 < size && candidates[i] == candidates[i+1]) i += 1;
+        solve(candidates, i+1, target, list);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        this.arr = candidates;
-        int n = candidates.length;
-        Arrays.sort(arr);
-        solve(0, n, target, new ArrayList<>());
+        this.size = candidates.length;
+        
+        Arrays.sort(candidates);
+        solve(candidates, 0, target, new ArrayList<>());
         
         return combination;
     }
