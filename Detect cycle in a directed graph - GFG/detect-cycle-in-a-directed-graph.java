217e9e36c3c85cb0f4991bc6daf1ae031f822aa5
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-12-02 23:13:31 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.util.*;
 import java.io.*;
 import java.lang.*;
@@ -12,7 +12,7 @@ class DriverClass {
             ArrayList<ArrayList<Integer>> list = new ArrayList<>();
             int V = sc.nextInt();
             int E = sc.nextInt();
-            for (int i = 0; i < V + 1; i++)
+            for (int i = 0; i < V; i++)
                 list.add(i, new ArrayList<Integer>());
             for (int i = 0; i < E; i++) {
                 int u = sc.nextInt();
@@ -25,102 +25,42 @@ class DriverClass {
                 System.out.println("0");
         }
     }
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 /*Complete the function below*/
 
 class Solution {
-    // public boolean detectCycle(ArrayList<ArrayList<Integer>> adj, 
-    // HashMap<Integer, Boolean> map, int curr){
-    //     if(map.get(curr) == true)return true;
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
         
-    //     map.replace(curr, true);
-    //     boolean flag = false;
+        boolean visited[] = new boolean[V];
+        boolean cyclicVisit[] = new boolean[V];
         
-    //     for(int i = 0; i<adj.get(curr).size(); i++){
-            
-    //         flag = detectCycle(adj, map, adj.get(curr).get(i));
-    //         if(flag == true)return true;
-    //     }
-    //         map.replace(curr, false);
-    //     return false;
-    // }
-    
-    // // Function to detect cycle in a directed graph.
-    // public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        
-        
-
-    //     HashMap<Integer, Boolean> map = new HashMap<>();
-    //     for(int i = 0; i<V; i++){
-    //         map.put(i, false);
-    //     }
-        
-    //     boolean flag = false;
-    //     // System.out.print("gvhgg"+adj.get(1).size()+"bjj");
-    //     for(int i = 0; i<V; i++){
-    //         map.replace(i, true);
-    //         for(int j = 0; j<adj.get(i).size(); j++){
-                
-    //             flag = detectCycle(adj, map, adj.get(i).get(j));
-    //             if(flag == true)return true;
-                
-    //         }
-            
-    //         map.replace(i, false);
-    //     }
-    //     return false;
-    // }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-public boolean dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int i) {
-    if(visited[i] == 1)return true;
-    
-    if(visited[i] == 2)return false;
-    
-    visited[i] = 1;
-    
-    for(int edge : adj.get(i)) {
-        if( dfs(adj, visited, edge) )return true;
-    }
-    
-    visited[i] = 2;
-    return false;
-}
-    
-    
-    
-public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj){
-    int[] visited = new int[V];
-    
-    for(int i=0; i<V; i++) {
-        if( visited[i] == 0) {
-            if(dfs(adj, visited, i))return true;
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                if(dfs(adj, i, visited, cyclicVisit))return true;
+            }
         }
         
+        return false;
     }
-    return false;
-}
     
-    
-    
-    
-    
-    
-    
-    
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj, int node, boolean visited[], boolean cyclicVisit[]){
+        
+        visited[node] = true;
+        cyclicVisit[node] = true;
+        
+        for(int next : adj.get(node)){
+            if(!visited[next]){
+                if(dfs(adj, next, visited, cyclicVisit))return true;
+            }
+            else if(visited[next] && cyclicVisit[next])return true;
+        }
+        
+        cyclicVisit[node] = false;
+        return false;
+    }
 }
