c9cc57916102d103cff3c7798df5d8c4b83914a9
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-12-07 16:09:48 +0530

    Time: 3 ms (50.58%), Space: 45 MB (21.45%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int num: nums){
+            map.put(num, false);
+        }
+        
+        
+        List<List<Integer>> perm = new ArrayList<>();
+        solve(nums, perm, new ArrayList<Integer>(), map);
+        
+        return perm;
+    }
+    
+    public void solve(int nums[], List<List<Integer>> perm, List<Integer> list, HashMap<Integer, Boolean> map){
+        if(list.size() == nums.length){
+            perm.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for(int i=0; i<nums.length; i++){
+            if(!map.get(nums[i])){
+                map.put(nums[i], true);
+                list.add(nums[i]);
+                solve(nums, perm, list, map);
+                list.remove(list.size()-1);
+                map.put(nums[i], false);
+            }
+        }
+    }
+}
