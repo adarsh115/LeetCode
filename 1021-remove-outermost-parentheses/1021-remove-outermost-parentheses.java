f3a0ed14d9fccb57a155f35ab4f2457fa008203c
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-12-13 00:30:01 +0530

    Time: 145 ms (5.24%), Space: 42.8 MB (53.50%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -5,12 +5,14 @@ class Solution {
         int count = 0;
         for(int i=0; i<s.length(); i++){
             char bracket = s.charAt(i);
-            
+            System.out.println(count);
             if(bracket == '(' && count++ > 0){
                 sb.append('(');
+                 System.out.println('(');
             }
             if(bracket == ')' && count-- > 1){
                 sb.append(')');   
+                 System.out.println(')');
             }
             
         }
